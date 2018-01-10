8d4e47ee32280352e8b675fd9476e7a3272fe6b0
ruko <rukokarasu@gmail.com>
2018-01-09 22:32:55 -0800

    first commit

---
+++ assignment-1/Ex2.c
@@ -0,0 +1,49 @@
+/* CS260 - Assignment 1 - EX 2*/
+/* Name: Ethan Jones
+ * Date: 9-1-2018
+ * Solution description: More pointer stuff
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+
+int bar(int* a, int* b, int c){
+    /*Set a to double its original value*/
+    *a *= 2;
+    
+    /*Set b to half its original value*/
+    *b /= 2;
+    
+    /*Assign a+b to c*/
+    c = *a + *b;
+    
+    /*Return c*/
+    return c;
+}
+
+int main(){
+    /*Declare three integers x,y and z and initialize them to 11, 12, & 13 respectively*/
+    int x = 11;
+    int y = 12;
+    int z = 13;
+    
+    /*Print the values of x, y and z*/
+    printf("x: %d\n", x);
+    printf("y: %d\n", y);
+    printf("z: %d\n", z);
+    
+    /*Call bar() appropriately, passing x,y,z as parameters*/
+    /*Print the value returned by bar from this function call*/
+    printf("Bar(x,y,z) returns %d\n", bar(&x, &y, z));
+    
+    /*Print the values of x, y and z again*/
+    printf("x: %d, y: %d, z: %d.\n", x, y, z);
+ 
+    /*Is the return value different than the value of z?  Why?
+	Express your short answer at the bottom of the file as a comment.*/
+    return 0;
+}
+    
+/* The return value is different because z was not passed as a pointer so bar
+ * doesn't have write access to it. 
+ */
---
+++ assignment-1/Ex3.c
@@ -0,0 +1,102 @@
+/* CS260 - Assignment 1 - EX 3*/
+/* Name: Ethan Jones
+ * Date: 9-1-2018
+ * Solution description: Extra more pointer stuff
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#define COUNT 10
+
+/*Sex: (M)ale or (F)email*/
+struct person {
+    int age;
+    char sex;
+};
+
+struct person* allocate();
+void generate(struct person* people);
+void output(struct person* people);
+void stats(struct person* people);
+void deallocate(struct person* people);
+
+
+int main() {
+    srand(time(NULL));
+
+    struct person* people = NULL;
+
+    /*call allocate
+	set people to the pointer returned by the function call*/
+    people = allocate();
+
+    /*call generate*/
+    generate(people);
+
+    /*call output*/
+    output(people);
+
+    /*call stats*/
+    stats(people);
+
+    /*call deallocate*/
+    deallocate(people);
+
+    return 0;
+}
+
+struct person* allocate() {
+    /*Allocate memory for ten persons - use malloc*/
+    struct person* ten_persons = (struct person*) malloc(COUNT * sizeof(struct person));
+
+    /*return the pointer*/
+    return ten_persons;
+}
+
+void generate(struct person* people) {
+    /*Generate random age and sex for 10 persons and store in people
+	Age is between 18 and 90 - sex is either M or F
+	Calculate random numbers between using rand().
+	Simply assigning ages 19, 20, 21.. is not sufficient*/
+    int i;
+    for (i = 0; i < COUNT; i++) {
+        people[i].age = rand() % (90 - 18) + 18;
+        people[i].sex = rand() % 2 == 0 ? 'M' : 'F';
+    }
+}
+
+void output(struct person* people) {
+    /*Output information about the ten persons in the format:
+	Person 1: Age: varAge Sex: varSex
+	...
+	Person 10: Age: varAge Sex: varSex*/
+    int i;
+    for (i = 0; i < COUNT; i++) {
+        printf("Person %d: Age: %d Sex: %c\n", i + 1, people[i].age, people[i].sex);
+    }
+}
+
+void stats(struct person* people) {
+    /*Compute and print the minimum, maximum and average age of the ten persons*/
+    int min = people[0].age;
+    int max = people[0].age;
+    float avg = 0;
+    int i;
+    for (i = 0; i < COUNT; i++) {
+        min = people[i].age < min ? people[i].age : min;
+        max = people[i].age > max ? people[i].age : max;
+        avg += people[i].age;
+    }
+    avg /= COUNT;
+    printf("min: %d\n", min);
+    printf("max: %d\n", max);
+    printf("avg: %f\n", avg);
+}
+
+void deallocate(struct person* people) {
+    /*Deallocate memory from people by calling free()*/
+    free(people);
+}
