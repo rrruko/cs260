f5b4446c33ef1e68a8402f45388e112ae3dea1ad
ruko <rukokarasu@gmail.com>
2018-01-14 22:58:06 -0800

    Array length is a precondition

--- assignment-1/Ex3.c
+++ assignment-1/Ex3.c
@@ -11,7 +11,7 @@
 
 #define COUNT 10
 
-/*Sex: (M)ale or (F)email*/
+/* Sex: (M)ale or (F)emale */
 struct person {
     int age;
     char sex;
@@ -28,46 +28,48 @@ int main() {
 
     struct person* people = NULL;
 
-    /*call allocate
-	set people to the pointer returned by the function call*/
+    /* call allocate
+	set people to the pointer returned by the function call */
     people = allocate();
 
-    /*call generate*/
+    /* call generate */
     generate(people);
 
-    /*call output*/
+    /* call output */
     output(people);
 
-    /*call stats*/
+    /* call stats */
     stats(people);
 
-    /*call deallocate*/
+    /* call deallocate */
     deallocate(people);
 
     return 0;
 }
 
 /* Description: Allocates COUNT persons using malloc.
- * Preconditions: None
+ * Preconditions: There must be enough free memory to perform the allocation.
  * Postconditions: Returns an array of uninitialized persons.
  */
 struct person* allocate() {
-    /*Allocate memory for ten persons - use malloc*/
-    struct person* ten_persons = (struct person*) malloc(COUNT * sizeof(struct person));
+    /* Allocate memory for ten persons - use malloc */
+    struct person* ten_persons = 
+        (struct person*) malloc(COUNT * sizeof(struct person));
 
-    /*return the pointer*/
+    /* return the pointer */
     return ten_persons;
 }
 
 /* Description: Write random persons to an array.
- * Preconditions: None (the array is completely overwritten and not read)
- * Postconditions: All persons have an age between 18 and 90 and a sex of M or F.
+ * Preconditions: `people` must be an array of length COUNT.
+ * Postconditions: All persons have an age between 18 and 90 and a sex of M or
+ * F.
  */
 void generate(struct person* people) {
-    /*Generate random age and sex for 10 persons and store in people
-	Age is between 18 and 90 - sex is either M or F
-	Calculate random numbers between using rand().
-	Simply assigning ages 19, 20, 21.. is not sufficient*/
+    /* Generate random age and sex for 10 persons and store in people
+     * Age is between 18 and 90 - sex is either M or F
+     * Calculate random numbers between using rand().
+     * Simply assigning ages 19, 20, 21.. is not sufficient */
     int i;
     for (i = 0; i < COUNT; i++) {
         people[i].age = rand() % (90 - 18) + 18;
@@ -76,26 +78,28 @@ void generate(struct person* people) {
 }
 
 /* Description: Print an array of persons.
- * Preconditions: None
- * Postconditions: None (this does not mutate the array)
+ * Preconditions: `people` must be an array of length COUNT.
+ * Postconditions: None (this does not mutate the array).
  */
 void output(struct person* people) {
-    /*Output information about the ten persons in the format:
-	Person 1: Age: varAge Sex: varSex
-	...
-	Person 10: Age: varAge Sex: varSex*/
+    /* Output information about the ten persons in the format:
+     * Person 1: Age: varAge Sex: varSex
+     * ...
+     * Person 10: Age: varAge Sex: varSex */
     int i;
     for (i = 0; i < COUNT; i++) {
-        printf("Person %d: Age: %d Sex: %c\n", i + 1, people[i].age, people[i].sex);
+        printf("Person %d: Age: %d Sex: %c\n", i + 1, 
+            people[i].age, people[i].sex);
     }
 }
 
 /* Description: Print stats about an array of people.
- * Preconditions: None
- * Postconditions: None (this does not mutate the array)
+ * Preconditions: `people` must be an array of length COUNT.
+ * Postconditions: None (this does not mutate the array).
  */
 void stats(struct person* people) {
-    /*Compute and print the minimum, maximum and average age of the ten persons*/
+    /* Compute and print the minimum, maximum and average age of the ten
+     * persons */
     int min = people[0].age;
     int max = people[0].age;
     float avg = 0;
@@ -117,6 +121,6 @@ void stats(struct person* people) {
  * Postconditions: No array.
  */
 void deallocate(struct person* people) {
-    /*Deallocate memory from people by calling free()*/
+    /* Deallocate memory from people by calling free() */
     free(people);
 }
