094e8c478108fc038df328e810a6323b37992a21
ruko <rukokarasu@gmail.com>
2018-01-20 14:34:48 -0800

    Complete Ex3

--- assignment-2/Ex3.c
+++ assignment-2/Ex3.c
@@ -6,54 +6,114 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-struct person {
+#define bool int
+#define true 1
+#define false 0
+
+typedef struct person {
 	int age;
 	char sex;
-};
+} person;
 
-void sort(struct person* people, int n) {
-	/*Sort the given array of people by age, of length n*/
+void swap(person* people, int a, int b) {
+    person tmp = people[a];
+    people[a] = people[b];
+    people[b] = tmp;
+}
+
+// Bubble sort
+void sort(person* people, int n) {
+    /* Sort the given array of people by age, of length n */
+    bool swapped = false;
+    do {
+        swapped = false;
+        int i;
+        for (i = 0; i < n - 1; i++) {
+            if (people[i].age > people[i + 1].age) {
+                swap(people, i, i + 1);
+                swapped = true;
+            }
+        }
+    } while (swapped);
 }
 
 /*This function can be identical to the previous sort function, however it needs to return the total number of comparisons carried out in the sort.
 If you want to attempt to optimize the sort, then you are welcome to do so */
 
-int sortAnalysis(struct person* people, int n) {
-     /*Sort the given array of persons of length n
-	 Return the number of comparisons required to terminate the algorithm */
+int sortAnalysis(person* people, int n) {
+    /* Sort the given array of persons of length n
+	Return the number of comparisons required to terminate the algorithm */
+    /* Sort the given array of people by age, of length n */
+    int comp_count = 0;
+    bool swapped = false;
+    do {
+        swapped = false;
+        int i;
+        for (i = 0; i < n - 1; i++) {
+            comp_count++;
+            if (people[i].age > people[i + 1].age) {
+                swap(people, i, i + 1);
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    return comp_count;
 }
 
-void display(struct person* people, int n) {
-     /*Display information about the ten people in the format:
+void display(person* people, int n) {
+    /* Display information about the ten people in the format:
 	  Person 1: Age: X, Gender: Y
 	  ...
 	  Person N: Age: X, Gender: Y
-      */
+    */
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("Person %d: Age: %d, Gender: %c\n", i, 
+            people[i].age, people[i].sex);
+    }
 }
 
-int main(){
+int main() {
+    srand(time(NULL));
+
     /*Declare an integer n and assign it a value of 20.*/
+    int n = 20;
 
     /*Allocate memory for an array of n persons using malloc.*/
+    person* people = (person*) malloc(sizeof(person) * n);
 
     /*Fill this array with random persons with ages between 18 and 90 and random genders, using rand().*/
-
-	/*Pass this array along with n to the display() function.*/
-	printf("---------------------- \n");
-	
-    /*Pass this array along with n to the sort() function.*/
+    int i;
+    for (i = 0; i < n; i++) {
+        int a = rand() % (90 - 18) + 18;
+        int s = rand() % 2 == 0 ? 'M' : 'F';
+        person p = { a, s };
+        people[i] = p;
+    }
 
     /*Pass this array along with n to the display() function.*/
-	printf("---------------------- \n");
+    display(people, n);
+    printf("---------------------- \n");
 	
-	/*Pass this array along with n to the sortAnalysis() function.*/
-	
-	/*Display the number of comparisons the function returns.	Did it matter that the array was already sorted? Why? - Display your answer in comments below
-	Note - this will depend on your implementation. There is not one answer*/
-	printf("---------------------- \n");
+    /*Pass this array along with n to the sort() function.*/
+    sort(people, n);
+
+    /*Pass this array along with n to the display() function.*/
+    display(people, n);
+    printf("---------------------- \n");
+    
+    /*Pass this array along with n to the sortAnalysis() function.*/
+    printf("%d\n", sortAnalysis(people, n));
+    
+    /*Display the number of comparisons the function returns.	Did it matter that the array was already sorted? Why? - Display your answer in comments below
+    Note - this will depend on your implementation. There is not one answer*/
+    printf("---------------------- \n");
+
+    /* Bubble sort performs n comparisons on a sorted list of length n.
+     * It performs a lot more comparisons on unsorted lists.
+     */
 
     return 0;
 }
-
-
