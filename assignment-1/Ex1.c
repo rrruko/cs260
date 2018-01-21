817a14e26793a86a8af99eb8bd8b3c7817a132bf
ruko <rukokarasu@gmail.com>
2018-01-20 23:35:54 -0800

    Convert line endings to unix

--- assignment-1/Ex1.c
+++ assignment-1/Ex1.c
@@ -3,7 +3,7 @@
  * Date: 9-1-2018
  * Solution description: Pointer stuff
  */
- 
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,26 +12,26 @@ void foo(int* ptr);
 int main() {
     /*declare an integer x*/
     int x = 42069;
-    
+
     /*print the address of x*/
     printf("Address of x: %p\n", &x);
-    
+
     /*Call foo() with the address of x*/
     foo(&x);
-    
+
     /*print the value of x*/
     printf("Value of x: %d\n", x);
-    
+
     return 0;
 }
 
 void foo(int* ptr) {
     /*Print the value pointed to by ptr*/
-    printf("Value pointed to by ptr: %d\n", *ptr); 
-     
+    printf("Value pointed to by ptr: %d\n", *ptr);
+
     /*Print the address pointed to by ptr*/
     printf("Address pointed to by ptr: %p\n", ptr);
-     
+
     /*Print the address of ptr itself*/
     printf("Address of ptr: %p\n", &ptr);
 }
--- assignment-1/Ex2.c
+++ assignment-1/Ex2.c
@@ -3,20 +3,20 @@
  * Date: 9-1-2018
  * Solution description: More pointer stuff
  */
- 
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int bar(int* a, int* b, int c){
     /*Set a to double its original value*/
     *a *= 2;
-    
+
     /*Set b to half its original value*/
     *b /= 2;
-    
+
     /*Assign a+b to c*/
     c = *a + *b;
-    
+
     /*Return c*/
     return c;
 }
@@ -26,24 +26,24 @@ int main(){
     int x = 11;
     int y = 12;
     int z = 13;
-    
+
     /*Print the values of x, y and z*/
     printf("x: %d\n", x);
     printf("y: %d\n", y);
     printf("z: %d\n", z);
-    
+
     /*Call bar() appropriately, passing x,y,z as parameters*/
     /*Print the value returned by bar from this function call*/
     printf("Bar(x,y,z) returns %d\n", bar(&x, &y, z));
-    
+
     /*Print the values of x, y and z again*/
     printf("x: %d, y: %d, z: %d.\n", x, y, z);
- 
+
     /*Is the return value different than the value of z?  Why?
 	Express your short answer at the bottom of the file as a comment.*/
     return 0;
 }
-    
+
 /* The return value is different because z was not passed as a pointer so bar
- * doesn't have write access to it. 
+ * doesn't have write access to it.
  */
