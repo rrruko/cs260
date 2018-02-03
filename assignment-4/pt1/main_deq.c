b00616721e21cd6a9e32f88bdea5f7cbd80ab985
ruko <rukokarasu@gmail.com>
2018-02-03 00:10:01 -0800

    oof

--- assignment-4/pt1/main_deq.c
+++ assignment-4/pt1/main_deq.c
@@ -3,74 +3,88 @@
 #include "deq.h"
 
 void assertTrue(int predicate, char *message) {
-	printf("%s: ", message);
-	if (predicate)
-		printf("Pass \n");
-	else
-		printf("Fail \n");
+    printf("%s: ", message);
+    if (predicate) {
+        printf("Pass \n");
+    } else {
+        printf("Fail \n");
+    }
 }
 
 int main () {
-	struct Deque *testDeque;
-	testDeque = malloc(sizeof(struct Deque));
+    struct Deque *testDeque;
+    testDeque = malloc(sizeof(struct Deque));
 
-	printf("Testing Deque ADT based on Circularly-Doubly-Linked List \n");
-	/*Initialize the deque */
-	initDeque(testDeque);
+    printf("Testing Deque ADT based on Circularly-Doubly-Linked List \n");
+    /*Initialize the deque */
+    initDeque(testDeque);
 
-	/*Testing isEmpty */
-	printf("-----------Testing isEmptyDeque -----------\n");
-	assertTrue(isEmptyDeque(testDeque) == 1, "Testing isEmptyDeque");
+    /*Testing isEmpty */
+    printf("-----------Testing isEmptyDeque -----------\n");
+    assertTrue(isEmptyDeque(testDeque) == 1, "Testing isEmptyDeque");
 
     printf("-----------Testing addBackDeque-----------\n");
-	/*Add 3 to the back */
-	addBackDeque(testDeque, 3);
-	/*Add 4 to the back */
-	addBackDeque(testDeque, 4);
-	/*Add 5 to the back */
-	addBackDeque(testDeque, 5);
+    /*Add 3 to the back */
+    printf("Adding 3 to back\n");
+    addBackDeque(testDeque, 3);
+    /*Add 4 to the back */
+    printf("Adding 4 to back\n");
+    addBackDeque(testDeque, 4);
+    /*Add 5 to the back */
+    printf("Adding 5 to back\n");
+    addBackDeque(testDeque, 5);
 
-	/*Should print 3.0  4.0  5.0 */
-	printf("-----------Testing printDeque-----------\n");
-	printDeque(testDeque);
-	assertTrue(frontDeque(testDeque) == 3, "Testing front == 3 ");
-	assertTrue(backDeque(testDeque) == 5, "Testing back == 5 ");
+    /*Should print 3.0  4.0  5.0 */
+    printf("-----------Testing printDeque-----------\n");
+    printDeque(testDeque);
+    assertTrue(frontDeque(testDeque) == 3, "Testing front == 3 ");
+    assertTrue(backDeque(testDeque) == 5, "Testing back == 5 ");
 
-	/*Add 2 to the front */
-	printf("-----------Testing addFrontDeque-----------\n");
-	addFrontDeque(testDeque, 2);
-	/*Add 1 to the front */
-	addFrontDeque(testDeque, 1);
+    printf("-----------Testing addFrontDeque-----------\n");
+    /*Add 2 to the front */
+    printf("Adding 2\n");
+    addFrontDeque(testDeque, 2);
+    /*Add 1 to the front */
+    printf("Adding 1\n");
+    addFrontDeque(testDeque, 1);
 
-	/*Should print 1, 2, 3, 4, 5 */
-	printf("-----------Testing printDeque-----------\n");
-	printDeque(testDeque);
-    assertTrue(frontDeque(testDeque) == 1, "Testing front == 1 ");
-	assertTrue(backDeque(testDeque) == 5, "Testing back == 5 ");
-
-	printf("-----------Testing frontDeque AND backDeque -----------\n");
+    /*Should print 1, 2, 3, 4, 5 */
+    printf("-----------Testing printDeque-----------\n");
+    printDeque(testDeque);
     assertTrue(frontDeque(testDeque) == 1, "Testing front == 1 ");
     assertTrue(backDeque(testDeque) == 5, "Testing back == 5 ");
 
-	/*Remove the back of the deque */
-	printf("-----------Testing removeFrontDeque -----------\n");
-	removeFrontDeque(testDeque);
-	assertTrue(frontDeque(testDeque) == 2, "Testing front == 2 ");
-	/*Should print: 2, 3, 4, 5 */
-	printDeque(testDeque);
+    printf("-----------Testing frontDeque AND backDeque -----------\n");
+    assertTrue(frontDeque(testDeque) == 1, "Testing front == 1 ");
+    assertTrue(backDeque(testDeque) == 5, "Testing back == 5 ");
 
-	/*Remove the front of the deque */
-	printf("-----------Testing removeBackDeque -----------\n");
-	removeBackDeque(testDeque);
+    /*Remove the back of the deque */
+    printf("-----------Testing removeFrontDeque -----------\n");
+    removeFrontDeque(testDeque);
+    assertTrue(frontDeque(testDeque) == 2, "Testing front == 2 ");
+    /*Should print: 2, 3, 4, 5 */
+    printDeque(testDeque);
+
+    /*Remove the front of the deque */
+    printf("-----------Testing removeBackDeque -----------\n");
+    removeBackDeque(testDeque);
     assertTrue(backDeque(testDeque) == 4, "Testing back == 4 ");
-	/*Should print: 2, 3, 4 */
-	printDeque(testDeque);
+    /*Should print: 2, 3, 4 */
+    printDeque(testDeque);
 
-	/*Testing isEmpty */
-	printf("-----------Testing isEmptyDeque -----------\n");
-	assertTrue(isEmptyDeque(testDeque) == 0, "Testing isEmptyDeque");
+    /*Testing isEmpty */
+    printf("-----------Testing isEmptyDeque -----------\n");
+    assertTrue(!isEmptyDeque(testDeque), "Testing !isEmptyDeque");
 
-	freeDeque(testDeque);
+    printf("Removing 3 elements from testDeque\n");
+    removeBackDeque(testDeque);
+    removeBackDeque(testDeque);
+    removeBackDeque(testDeque);
 
-	return EXIT_SUCCESS;
+    assertTrue(isEmptyDeque(testDeque), "Testing isEmptyDeque");
+
+    addBackDeque(testDeque, 9);
+    freeDeque(testDeque);
+
+    return EXIT_SUCCESS;
 }
