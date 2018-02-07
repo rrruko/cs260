1ba933c13d091cb70915c090f6da3bf575d807e8
ruko <rukokarasu@gmail.com>
2018-02-06 23:48:00 -0800

    Start assignment 5

---
+++ assignment-5/skipList.c
@@ -0,0 +1,221 @@
+/*
+   Name: Ethan Jones
+   Date: 6-2-2018
+   Description: Skip lists
+*/
+/*
+    skip list implementation
+    link heights are probabilistically determined
+    link heights are only allowed to exceed current max (currMax) height by one
+    link heights are capped at maximum level (maxLevel)
+*/
+
+#include "skipList.h"
+#include <malloc.h>
+
+/*
+    initSkipList: Skiplist is initialized
+    pre: list is not null
+    pre: max is at least 1 max. should be set to log n of the number of level 1
+    links
+    param1: list - the list
+    param2: max - maximum depth of the list
+    post: memory is allocated to sentinel
+    maxLevel, currMax and size are initialized
+*/
+void initSkipList(struct skipList *list, int max) {
+    assert(list != NULL);
+    assert(max > 0);
+    /* sentinel is an array with a pointer to every level */
+    list->sentinel = malloc(sizeof(struct sLink)+sizeof(struct sLink)*max+1);
+    list->maxLevel = max;/* max allowable levels */
+    list->currMax = 0;/* level of highest node currently */
+    list->size = 0;/* number of level 1 links */
+    
+    int i;
+    for(i = max; i > 0; i--) 
+    {
+        /* sentinel points to itself at every level */
+        list->sentinel->next[i] = list->sentinel;
+    }
+}
+
+/*
+    levelGen: Generate a random level for new links
+    pre: list is not null
+    param1: list - the list
+    return: random level
+    post: random level cannot exceed list->maxLevel
+    random level can only exceed currMax by 1
+    random levels are determined by a "series of coin flips"
+    Each level is 50% less common than the last
+    Approximately 50% are level 2, 25% are level 3, 12.5% are level 4, etc
+    Each consecutive "heads" adds one to the level, at the first "tails" we
+    have reached our level
+    start at level 1 and add 1 for each heads
+    Ex: HHT = level 3, T = 1, THH = 1, HHHT = 4
+*/
+int levelGen(struct skipList *list) {
+    assert(list != NULL);
+    /*  here's a cute alternate way to define this procedure without
+     *  looping!
+     *      double r = (double)rand() / RAND_MAX;
+     *      int level = 1 + (int)log2(1.0/r);
+     *      return fmin(level, maxLevel);
+     *  This works because a number chosen uniformly from the range (0, 1] has
+     *  a 1/2^n chance of being less than 1/2^n, or, equivalently, has a 1/2^n
+     *  chance that the log of its reciprocal is greater than n.
+     */
+    int level = 1;
+    // Side-affecting loop condition! 
+    while (rand() % 2 == 0
+        && level < list->maxLevel
+        && level < list->currMax + 1) {
+        level++;
+    }
+    return level;
+}
+
+/*
+    addValTest: Adds a value to the skipList at a specified level - useful for
+    testing purposes
+    pre: list is not null
+    pre: lvl > 0 and lvl <= maxLevel
+    param1: list - the list
+    param2: val - the value to add
+    param3: lvl - the level of the link
+    post: value is added to the skip list at each necessary level
+*/
+void addValTest(struct skipList *list, TYPE val, int lvl) {
+    assert(list != NULL);
+    assert(lvl <= list->maxLevel && lvl > 0);
+    
+    /* if lvl is greater than currMax, set currMax to lvl - this is just list
+     * maintenance */
+    if(lvl > list->currMax) {
+        list->currMax = lvl;
+    }
+    
+    /* pointers for node to insert and iterator node */
+    struct sLink *insert, *curr;
+    /* allocate memory for link to insert - extra space is allocated for the 
+     * "height" of the link */
+    insert = malloc(sizeof(struct sLink) + sizeof(struct sLink)*lvl);
+    
+    /* set up node values*/
+    insert->value = val;
+    insert->level = lvl;
+    
+    /* initialize curr to sentinel - start at the head of the list */
+    curr = list->sentinel;
+
+    while (insert->value > curr->next->value) {
+        curr = curr->next;
+    }
+    // curr is now the first node such that curr->next->value is not less than
+    // insert->value, so we should put insert after curr and before curr->next
+
+    
+    /* FIX ME */
+    /* add insert to the correct position for each level of the skip list less
+     * or equal to the link's level */
+    /* HINT - this is simply a test function - don't hesitate to put debugging
+     * statements in to help navigate */
+    /* Start at the top and work down */
+    printf("inserting %d as a lvl %d node\n", val, lvl);
+}
+
+/*
+    addVal: Adds a value to the skipList at a random level
+    pre: list is not null
+    pre: lvl > 0 and lvl <= maxLevel
+    param1: list - the list
+    param2: val - the value to add
+    post: value is added to the skip list at each necessary level. 
+    Level is generated randomly.
+    HINT - this is the same process as addValTest with one exception. You 
+    need to generate a level using LevelGen()
+*/
+void addVal(struct skipList *list, TYPE val) {
+    /* FIX ME */
+    
+    
+    
+}
+
+/*
+    searchVal: Searches the skipList for a value
+    pre: list is not null
+    param1: list - the list
+    param2: val - the search value
+    post: return an integer greater than 0 if found - If not found, return 0
+    Returns the number of comparisons  needed to find the value if found (will
+    work as boolean return)
+*/
+int searchVal(struct skipList *list, TYPE val) {
+    /* FIX ME */
+    /* HINT - traversing the list is similar to the an insert */
+    
+    
+    return 0;
+}
+
+/*
+    printLv: Print the specified level of the list
+    pre: list is not null
+    pre: lv is greater than 0 and less than list->maxLevel
+    param1: list - the list
+    param2: lv - level of the list to print
+    post: prints all of the values at a specified level
+*/
+void printLv(struct skipList *list, int lv) {
+    /* FIX ME */
+    
+    
+    
+}
+
+/*
+    getSize: return the number of level 1 links in the skip list
+    pre: list is not null
+    param1: list - the list
+    post: return the number of level 1 links in the list
+*/
+int getSize(struct skipList *list) {
+    int size = 0;
+    struct sLink* link = list->sentinel;
+    while (link->next != NULL) {
+        size++;
+        link = link->next[0];
+    }
+    return size;
+}
+
+/*
+    removeLink: Remove the first instance of the link containing the value from
+    the list
+    pre: list is not null
+    param1: list - the list
+    param2: val - value to remove
+    post: link is removed from the list and pointers are adjusted
+    post: if not found "Not Found" is displayed via standard output
+*/
+void removeLink(struct skipList *list, TYPE val) {
+    /* FIX ME */
+    
+    
+    
+}
+
+/*
+    deleteList: delete all links from the list as well as the list
+    pre: list is not null
+    param1: list - the list
+    post: all links are deallocated
+    post: sLinks are deallocated
+*/
+void deleteList(struct skipList *list) {
+    /* FIX ME */
+    
+    
+}
---
+++ assignment-5/skipList_main.c
@@ -0,0 +1,105 @@
+#include "skipList.h"
+#include <malloc.h>
+
+int main() {
+	/* initialize list one - max depth 4 */
+	int cap = 4;
+	struct skipList *list;
+	list = malloc(sizeof(struct skipList) + sizeof(struct sLink)*cap);
+	initSkipList(list, cap);
+	
+	printf("\n-------*****------- Static Test -------*****-------\n");
+	staticTest(list);
+	
+	/* initialize list two - max depth 8  - comment & un-comment the test function accordingly*/
+	cap = 8;
+	struct skipList *list2;
+	list2 = malloc(sizeof(struct skipList) + sizeof(struct sLink)*cap+1);
+	initSkipList(list2, cap);
+	
+	printf("\n-------*****------- Dynamic Test -------*****-------\n");
+	test(list2);
+	
+	return 0;
+}
+
+/*
+	staticTest: Function tests the addValTest, printLv and removeLink functions
+	pre: list is not null
+	param1: list - the list
+	post: links are added to the list at varying levels
+	post: results are displayed via standard output
+*/
+void staticTest(struct skipList *list) {
+	printf("\n-------------- Testing addValTest --------------\n");
+	addValTest(list, 2, 1);
+	addValTest(list, 6, 1);
+	addValTest(list, 4, 1);
+	addValTest(list, 5, 2);
+	addValTest(list, 10, 2);
+	addValTest(list, 3, 2);
+	addValTest(list, 7, 1);
+	addValTest(list, 9, 4);
+	addValTest(list, 8, 3);
+	addValTest(list, 15, 3);
+	addValTest(list, 13, 1);
+	
+	printf("\n-------------- Testing printLv --------------\n");
+	printLv(list, 1);
+	/* should display: 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 15 */
+	printLv(list, 2);
+	/* should display: 3, 5, 8, 9, 10, 15 */
+	printLv(list, 3);
+	/* should display: 8, 9, 15 */
+	printLv(list, 4);
+	/* should display: 9 */
+	
+	printf("\n-------------- Testing searchVal --------------\n");
+	printf("Search for 4: returned: %d \n", searchVal(list,4));
+	printf("Search for 1: returned: %d \n", searchVal(list,1));
+	printf("Search for 15: returned: %d \n", searchVal(list,15));
+	
+	printf("\n-------------- Testing removeLink --------------\n");
+	printf("Removing 2 from list \n");
+	removeLink(list, 2);
+	printf("Removing 5 from list \n");
+	removeLink(list, 5);
+	
+	printf("\n-------------- Testing printLv --------------\n");
+	printLv(list, 1);
+	/* should display: 3, 4, 6, 7, 8, 9, 10, 13, 15 */
+	printLv(list, 2);
+	/* should display: 3, 8, 9, 10, 15 */
+	printf("\n-------*****------- Testing Complete -------*****-------\n");
+	
+}
+
+/*
+	test: Function tests the addVal, printLv and removeLink functions with a random, large set
+	pre: list is not null
+	param1: list - the list
+	post: links are added to the list at varying levels
+	post: results are displayed via standard output
+*/
+void test(struct skipList *list) {
+	int i = 0;
+	
+	while(i < 25) {
+		addVal(list, rand()%1000);
+		i++;
+	}
+	
+	printLv(list, 1);
+	printLv(list, 2);
+	printLv(list, 3);
+	printLv(list, 4);
+	printLv(list, 5);
+	printLv(list, 6);
+	printLv(list, 7);
+	printLv(list, 8);
+	
+	printf("Search for 50: returned: %d \n", searchVal(list, 50));
+	printf("Search for 0: returned: %d \n", searchVal(list, 0));
+	printf("Search for 195: returned: %d \n", searchVal(list, 195));
+	/* Verify your own results */
+}
