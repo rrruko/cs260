2b9be5ea2f6abc73e50fe633665aca6b34d6b2a1
ruko <rukokarasu@gmail.com>
2018-02-25 17:11:36 -0800

    Convert tabs to spaces, unix line endings

--- assignment-7/dynamicArray.c
+++ assignment-7/dynamicArray.c
@@ -10,216 +10,216 @@
     param: right - second task
     pre: none
     post: none
-	return: return scheme is below
+    return: return scheme is below
         -1 if priority of left < priority of right
         1 if priority of left > priority of right
         0 if priority of left = priority of right
 */
 int compare(TYPE left, TYPE right) {
-	if (left.priority < right.priority)
-		return -1;
-	else if (left.priority > right.priority)
-		return 1;
-	else
-		return 0;
+    if (left.priority < right.priority)
+        return -1;
+    else if (left.priority > right.priority)
+        return 1;
+    else
+        return 0;
 }
 
 /*
-	initDynArr: Initialize (including allocation of data array) dynamic array.
-	param1: d - pointer to the dynamic array
-	param2 : capacity - capacity of the dynamic array
-	pre: d is not null
-	post: internal data array can hold cap elements
-	post: d->data is not null
+    initDynArr: Initialize (including allocation of data array) dynamic array.
+    param1: d - pointer to the dynamic array
+    param2 : capacity - capacity of the dynamic array
+    pre: d is not null
+    post: internal data array can hold cap elements
+    post: d->data is not null
 */
 void initDynArr(struct DynArr *d, int capacity) {
-	assert(capacity > 0);
-	assert(d != NULL);
-	d->data = (TYPE *) malloc(sizeof(TYPE) * capacity);
-	assert(d->data != NULL);
-	d->size = 0;
-	d->capacity = capacity;
+    assert(capacity > 0);
+    assert(d != NULL);
+    d->data = (TYPE *) malloc(sizeof(TYPE) * capacity);
+    assert(d->data != NULL);
+    d->size = 0;
+    d->capacity = capacity;
 }
 
 /*
-	createDynArr: Allocate and initialize dynamic array.
-	param: cap desired capacity for the dynamic array
-	pre: cap is greater than 0
-	post: none
-	ret: a non-null pointer to a dynArr of cap capacity and 0 elements in it.
+    createDynArr: Allocate and initialize dynamic array.
+    param: cap desired capacity for the dynamic array
+    pre: cap is greater than 0
+    post: none
+    ret: a non-null pointer to a dynArr of cap capacity and 0 elements in it.
 */
 struct DynArr *createDynArr(int cap) {
-	assert(cap > 0);
-	struct DynArr *dyn;
-	dyn = malloc(sizeof(struct DynArr));
-	if(!dyn) {
-		printf("Failed to allocate memory \n");
-	}
-	initDynArr(dyn,cap);
-	return dyn;
+    assert(cap > 0);
+    struct DynArr *dyn;
+    dyn = malloc(sizeof(struct DynArr));
+    if(!dyn) {
+        printf("Failed to allocate memory \n");
+    }
+    initDynArr(dyn,cap);
+    return dyn;
 }
 
 /*
-	freeDynArr: Deallocate data array in dynamic array.
-	param: d - pointer to the dynamic array
-	pre: none
-	post: d->data points to null
-	post: size and capacity are 0
-	post: the memory used by d->data is freed
+    freeDynArr: Deallocate data array in dynamic array.
+    param: d - pointer to the dynamic array
+    pre: none
+    post: d->data points to null
+    post: size and capacity are 0
+    post: the memory used by d->data is freed
 */
 void freeDynArr(struct DynArr *d) {
-	if(d->data != NULL) {
-		free(d->data); /* free the space on the heap */
-		d->data = NULL; /* make it point to null */
-	}
-	d->size = 0;
-	d->capacity = 0;
+    if(d->data != NULL) {
+        free(d->data); /* free the space on the heap */
+        d->data = NULL; /* make it point to null */
+    }
+    d->size = 0;
+    d->capacity = 0;
 }
 
 /*
-	deleteDynArr: Deallocate data array and the dynamic array.
-	param: d - pointer to the dynamic array
-	pre: d is not NULL
-	post: the memory used by d->data is freed
-	post: the memory used by d is freed
+    deleteDynArr: Deallocate data array and the dynamic array.
+    param: d - pointer to the dynamic array
+    pre: d is not NULL
+    post: the memory used by d->data is freed
+    post: the memory used by d is freed
 */
 void deleteDynArr(struct DynArr *d) {
-	assert(d != NULL);
-	freeDynArr(d);
-	free(d);
+    assert(d != NULL);
+    freeDynArr(d);
+    free(d);
 }
 
 /*
-	sizeDynArr: Get the size of the dynamic array
-	param: d - pointer to the dynamic array
-	pre: d is not null
-	post: none
-	ret: the size of the dynamic array
+    sizeDynArr: Get the size of the dynamic array
+    param: d - pointer to the dynamic array
+    pre: d is not null
+    post: none
+    ret: the size of the dynamic array
 */
 int sizeDynArr(struct DynArr *d) {
-	assert(d != NULL);
-	return d->size;
+    assert(d != NULL);
+    return d->size;
 }
 
 /*
-	getDynArr: Get an element from the dynamic array from a specified position
-	param1: d - pointer to the dynamic array
-	param2: pos - integer index to get the element from
-	pre: d is not null
-	pre: d is not empty
-	pre: pos <= size of the dynamic array and >= 0
-	post: no changes to the dynamic array
-	return: value stored at index position, pos
+    getDynArr: Get an element from the dynamic array from a specified position
+    param1: d - pointer to the dynamic array
+    param2: pos - integer index to get the element from
+    pre: d is not null
+    pre: d is not empty
+    pre: pos <= size of the dynamic array and >= 0
+    post: no changes to the dynamic array
+    return: value stored at index position, pos
 */
 TYPE getDynArr(struct DynArr *d, int pos) {
-	assert(d != NULL);
-	assert(pos <= d->size);
-	assert(pos >= 0);
+    assert(d != NULL);
+    assert(pos <= d->size);
+    assert(pos >= 0);
 
-	return d->data[pos];
+    return d->data[pos];
 }
 
 /*
     _dynArrSetCapacity: Resizes the old array to the size of capacity
     param1: d - pointer to the dynamic array
-	param2: newCap - the new desired capacity
-	pre: d is not null
-	post: d has capacity newCap
+    param2: newCap - the new desired capacity
+    pre: d is not null
+    post: d has capacity newCap
 */
 void _dynArrSetCapacity(struct DynArr *d, int newCap) {
     /* Create a new dynamic array with new capacity */
-	TYPE *newArr = malloc(sizeof(TYPE)*newCap);
-	assert(newArr != NULL);
+    TYPE *newArr = malloc(sizeof(TYPE)*newCap);
+    assert(newArr != NULL);
 
-	int i;
+    int i;
     for(i = 0; i < d->size; i++){
-		newArr[i] = d->data[i];
-	}
+        newArr[i] = d->data[i];
+    }
 
-	free(d);
-	d->data = newArr;
-	d->capacity = newCap;
-	d->size = i;
+    free(d);
+    d->data = newArr;
+    d->capacity = newCap;
+    d->size = i;
 
 }
 
 /*
-	addDynArr: Adds an element to the end of the dynamic array
-	param: d - pointer to the dynamic array
-	param: val - the value to add to the end of the dynamic array
-	pre: the dynArry is not null
-	post: size increases by 1
-	post: if reached capacity, capacity is doubled
-	post: val is in the last utilized position in the array
+    addDynArr: Adds an element to the end of the dynamic array
+    param: d - pointer to the dynamic array
+    param: val - the value to add to the end of the dynamic array
+    pre: the dynArry is not null
+    post: size increases by 1
+    post: if reached capacity, capacity is doubled
+    post: val is in the last utilized position in the array
 */
 void addDynArr(struct DynArr *d, TYPE val) {
-	assert(d != NULL);
-	/* If full, then re-size */
-	if(d->size >= d->capacity) {
-		_dynArrSetCapacity(d, d->capacity * 2);
-	}
-	d->data[d->size] = val;
-	d->size++;
+    assert(d != NULL);
+    /* If full, then re-size */
+    if(d->size >= d->capacity) {
+        _dynArrSetCapacity(d, d->capacity * 2);
+    }
+    d->data[d->size] = val;
+    d->size++;
 }
 
 /*
-	putDynArr: Put an item into the dynamic array at the specified location, overwriting the element that was there
-	param1: d - pointer to the dynamic array
-	param2: pos - the index to put the value into
-	param3: val	- the value to insert
-	pre: d is not null
-	pre: d is not empty
-	pre: pos >= 0 and pos < capacity of the array
-	post: index (pos) contains new value (val)
+    putDynArr: Put an item into the dynamic array at the specified location, overwriting the element that was there
+    param1: d - pointer to the dynamic array
+    param2: pos - the index to put the value into
+    param3: val    - the value to insert
+    pre: d is not null
+    pre: d is not empty
+    pre: pos >= 0 and pos < capacity of the array
+    post: index (pos) contains new value (val)
 */
 void putDynArr(struct DynArr *d, int pos, TYPE val) {
-	assert(d != NULL);
+    assert(d != NULL);
     assert(pos >= 0);
-	assert(pos < d->capacity);
+    assert(pos < d->capacity);
 
-	d->data[pos] = val;
+    d->data[pos] = val;
 
 }
 
 /*
-	swapDynArr: Swap two specified elements in the dynamic array
-	param1: d - pointer to the dynamic array
-	param2: i - one of the elements to be swapped
-	param3: j - one of the elements to be swapped
-	pre: d is not null
-	pre: d is not empty
-	pre: i & j are greater than or equal to 0
-	pre: i & j are less or equal to the size of the dynamic array
-	post: index i now holds the value at j and index j now holds the value at i
+    swapDynArr: Swap two specified elements in the dynamic array
+    param1: d - pointer to the dynamic array
+    param2: i - one of the elements to be swapped
+    param3: j - one of the elements to be swapped
+    pre: d is not null
+    pre: d is not empty
+    pre: i & j are greater than or equal to 0
+    pre: i & j are less or equal to the size of the dynamic array
+    post: index i now holds the value at j and index j now holds the value at i
 */
 void swapDynArr(struct DynArr *d, int i, int  j) {
-	TYPE  temp;
-	assert(d != NULL);
-	assert(i <= d->size);
-	assert(j <= d->size);
-	assert(i >= 0);
-	assert(j >= 0);
+    TYPE  temp;
+    assert(d != NULL);
+    assert(i <= d->size);
+    assert(j <= d->size);
+    assert(i >= 0);
+    assert(j >= 0);
 
-	temp = d->data[i];
-	d->data[i] = d->data[j];
-	d->data[j] = temp;
+    temp = d->data[i];
+    d->data[i] = d->data[j];
+    d->data[j] = temp;
 }
 
 /*
-	removeAtDynArr: Remove the element at the specified location from the array, shifts other elements back one to fill the gap
-	param1: d - pointer to the dynamic array
-	param2: pos - location of element to remove
-	pre: d is not null
-	pre: d is not empty
-	pre: pos < size and pos >= 0
-	post: the element at pos is removed
-	post: the elements past pos are shifted back one
+    removeAtDynArr: Remove the element at the specified location from the array, shifts other elements back one to fill the gap
+    param1: d - pointer to the dynamic array
+    param2: pos - location of element to remove
+    pre: d is not null
+    pre: d is not empty
+    pre: pos < size and pos >= 0
+    post: the element at pos is removed
+    post: the elements past pos are shifted back one
 */
 void removeAtDynArr(struct DynArr *d, int idx) {
-	int i;
-	assert(d != NULL);
-	assert(idx < d->size);
-	assert(idx >= 0);
+    int i;
+    assert(d != NULL);
+    assert(idx < d->size);
+    assert(idx >= 0);
 
    /* Shift all elements up */
    for(i = idx; i < d->size-1; i++){
@@ -229,125 +229,125 @@ void removeAtDynArr(struct DynArr *d, int idx) {
 }
 
 /*
-	isEmptyDynArr: returns boolean based on whether the array is empty
-	param: d - pointer to the dynamic array
-	pre: d is not null
-	post: none
-	ret: 1 if empty, otherwise return 0
+    isEmptyDynArr: returns boolean based on whether the array is empty
+    param: d - pointer to the dynamic array
+    pre: d is not null
+    post: none
+    ret: 1 if empty, otherwise return 0
 */
 int isEmptyDynArr(struct DynArr *d) {
-	assert(d != NULL);
+    assert(d != NULL);
 
-	if(d->size == 0) {
-		return 1;
-	}
-	return 0;
+    if(d->size == 0) {
+        return 1;
+    }
+    return 0;
 }
 
 /*
-	copyDynArr: copy elements from a dynamic array to another dynamic array
-	param1: source - pointer to the source dynamic array
-	param2: destination - pointer to the destination dynamic array
-	pre: source is not null and s is not empty
-	post: destination is initialized
-	post: the elements from source are copied to destination
+    copyDynArr: copy elements from a dynamic array to another dynamic array
+    param1: source - pointer to the source dynamic array
+    param2: destination - pointer to the destination dynamic array
+    pre: source is not null and s is not empty
+    post: destination is initialized
+    post: the elements from source are copied to destination
 */
 void copyDynArr(struct DynArr *source, struct DynArr *destination) {
-	assert(source != NULL);
-	assert(source->size > 0);
+    assert(source != NULL);
+    assert(source->size > 0);
 
-	initDynArr(destination, source->capacity);
+    initDynArr(destination, source->capacity);
 
-	int i;
-	for(i = 0; i < source->size; i++) {
-		destination->data[i] = source->data[i];/* copy elements to destination array */
-	}
+    int i;
+    for(i = 0; i < source->size; i++) {
+        destination->data[i] = source->data[i];/* copy elements to destination array */
+    }
 
-	destination->size = source->size;
+    destination->size = source->size;
 }
 
 /*
-	Heap-based priority queue implementation ----------------------------------- Starts here --------------------------------------
+    Heap-based priority queue implementation ----------------------------------- Starts here --------------------------------------
 */
 
 /*
-	getMinHeap: Get the first node in the heap (root)
-	param: heap - pointer to the heap
-	pre: heap is not empty
-	post: no changes to the heap
-	return: value of first node's value
+    getMinHeap: Get the first node in the heap (root)
+    param: heap - pointer to the heap
+    pre: heap is not empty
+    post: no changes to the heap
+    return: value of first node's value
 */
 TYPE getMinHeap(struct DynArr *heap) {
-	/* FIXME */
-	
-	
+    /* FIXME */
+    
+    
 }
 
 /*
-	_smallerIndexHeap: Get the index of the smaller node between two nodes in a heap - this is an auxiliary function
-	param1: heap - pointer to the heap
-	param2: i - index of one node
-	param3: j - index of the other node
-	pre: i < size and j < size
-	return: the index of the smaller node
+    _smallerIndexHeap: Get the index of the smaller node between two nodes in a heap - this is an auxiliary function
+    param1: heap - pointer to the heap
+    param2: i - index of one node
+    param3: j - index of the other node
+    pre: i < size and j < size
+    return: the index of the smaller node
 */
 int _smallerIndexHeap(struct DynArr *heap, int i, int j) {
-	/* FIXME */
-	
-	
+    /* FIXME */
+    
+    
 }
 
 /*
-	_siftUp: Compare index to it's parent and swap it up the heap until it is in place
-	param1: heap - pointer to the heap
-	param2: index - the index of the child node (to be swapped)
-	pre: heap is not null
-	pre: index < heap->size
-	post: heap properties are maintained
+    _siftUp: Compare index to it's parent and swap it up the heap until it is in place
+    param1: heap - pointer to the heap
+    param2: index - the index of the child node (to be swapped)
+    pre: heap is not null
+    pre: index < heap->size
+    post: heap properties are maintained
 */
 void _siftUp(struct DynArr *heap, int index) {
-	/* FIXME */
-	
-	
+    /* FIXME */
+    
+    
 }
 
 /*
-	_siftDown: Compare index to it's children and swap with the smaller element
-	param1: heap - pointer to the heap
-	param2: index - the index of the parent node (to be swapped)
-	pre: heap is not null
-	pre: index < floor((heap->size-1)/2)
-	post: heap properties are maintained
+    _siftDown: Compare index to it's children and swap with the smaller element
+    param1: heap - pointer to the heap
+    param2: index - the index of the parent node (to be swapped)
+    pre: heap is not null
+    pre: index < floor((heap->size-1)/2)
+    post: heap properties are maintained
 */
 void _siftDown(struct DynArr *heap, int index) {
-	/* FIXME */
-	
-	
+    /* FIXME */
+    
+    
 }
 
 /*
-	addHeap: Add a node to the heap in last position - call siftUp() to get the node in place
-	param1: heap - pointer to the heap
-	param2: val - node to be added to the heap
-	pre: heap is not null
-	post: node is added to the heap (maintain heap properties)
-	post: heap size is incremented
+    addHeap: Add a node to the heap in last position - call siftUp() to get the node in place
+    param1: heap - pointer to the heap
+    param2: val - node to be added to the heap
+    pre: heap is not null
+    post: node is added to the heap (maintain heap properties)
+    post: heap size is incremented
 */
 void addHeap(struct DynArr *heap, TYPE val) {
-	/* FIXME */
-	
-	
+    /* FIXME */
+    
+    
 }
 
 /*
-	removeMinHeap: Remove the first node from the heap -  call siftDown() to get the node in place
-	param: heap - pointer to the heap
-	pre: heap is not empty
-	post: the first node is removed from the heap, the last node is promoted to root - use siftDown() to maintain the heap
-	HINT: use the _smallerIndexHeap to find the smaller node of the current node
+    removeMinHeap: Remove the first node from the heap -  call siftDown() to get the node in place
+    param: heap - pointer to the heap
+    pre: heap is not empty
+    post: the first node is removed from the heap, the last node is promoted to root - use siftDown() to maintain the heap
+    HINT: use the _smallerIndexHeap to find the smaller node of the current node
 */
 void removeMinHeap(struct DynArr *heap) {
-	/* FIXME */
-	
-	
+    /* FIXME */
+    
+    
 }
--- assignment-7/main_heap.c
+++ assignment-7/main_heap.c
@@ -1,6 +1,6 @@
 /*
-	file: main_heap.c
-	Heap test file
+    file: main_heap.c
+    Heap test file
 */
 
 #include <stdio.h>
@@ -10,127 +10,127 @@
 #include "dynamicArray.h"
 
 /*
-	printTestResult: function to print the result of a test function
-	param1: predicate - the result of the test
-	param2: nameTestFunction - name of the function being tested
-	param3: message - message passed to the test function
-	post: result is printed to standard output
+    printTestResult: function to print the result of a test function
+    param1: predicate - the result of the test
+    param2: nameTestFunction - name of the function being tested
+    param3: message - message passed to the test function
+    post: result is printed to standard output
 */
 void printTestResult(int predicate, char *nameTestFunction, char *message) {
-	if (predicate) {
-	   printf("%s() - %s: PASS\n",nameTestFunction, message);
-	} else {
-	   printf("%s() - %s: FAIL\n",nameTestFunction, message);
-	}
+    if (predicate) {
+       printf("%s() - %s: PASS\n",nameTestFunction, message);
+    } else {
+       printf("%s() - %s: FAIL\n",nameTestFunction, message);
+    }
 }
 
 /*
-	createTask: create a task from the description and the priority
-	param: priority - priority of the task
-	param: desc - pointer to the description string
-	pre: none
-	post: none
-	return: a task with description and priority
+    createTask: create a task from the description and the priority
+    param: priority - priority of the task
+    param: desc - pointer to the description string
+    pre: none
+    post: none
+    return: a task with description and priority
 */
 struct task createTask(int priority, char *desc) {
-	struct task newTask;
-	newTask.priority = priority;
-	strcpy(newTask.description, desc);
-	return newTask;
+    struct task newTask;
+    newTask.priority = priority;
+    strcpy(newTask.description, desc);
+    return newTask;
 }
 
 /*
-	printTask: Display the information about the first node in the heap (root)
-	param: task - the task to print
-	pre: heap is not empty
-	post: no changes to the heap
-	return: the task priority and description are printed to standard output
-	format: "Priority: XX - Description: Ipsum Lorem"
+    printTask: Display the information about the first node in the heap (root)
+    param: task - the task to print
+    pre: heap is not empty
+    post: no changes to the heap
+    return: the task priority and description are printed to standard output
+    format: "Priority: XX - Description: Ipsum Lorem"
 */
 void printTask(TYPE task) {
-	printf("Priority: %d - Description: %s \n",task.priority, task.description);
+    printf("Priority: %d - Description: %s \n",task.priority, task.description);
 }
 
 /*
-	printList: Display the information about the tasks in ascending order
-	param: heap - the heap full of tasks to print
-	pre: heap is not empty
-	post: no changes to the heap
-	return: the task priority and description are printed to standard output
-	format: "Priority: XX - Description: Ipsum Lorem" - use the printTask function
+    printList: Display the information about the tasks in ascending order
+    param: heap - the heap full of tasks to print
+    pre: heap is not empty
+    post: no changes to the heap
+    return: the task priority and description are printed to standard output
+    format: "Priority: XX - Description: Ipsum Lorem" - use the printTask function
 */
 void printList(struct DynArr *heap) {
-	/* FIXME */
-	
-	
+    /* FIXME */
+    
+    
 }
 
 int main() {
-	struct task task1, task2, task3, task4, task5, task6, task7, task8, task9, task10;
-	struct DynArr *taskList = malloc(sizeof(struct DynArr));
-	initDynArr(taskList, 16);
+    struct task task1, task2, task3, task4, task5, task6, task7, task8, task9, task10;
+    struct DynArr *taskList = malloc(sizeof(struct DynArr));
+    initDynArr(taskList, 16);
 
-	/* create tasks */
-	task1 = createTask(10, "Clean house");
-	task2 = createTask(6, "Eat dinner");
-	task3 = createTask(2, "Do laundry");
-	task4 = createTask(4, "Do dishes");
-	task5 = createTask(5, "Grocery shopping");
-	task6 = createTask(7, "Change oil");
-	task7 = createTask(13, "Do taxes");
-	task8 = createTask(8, "Sleep");
-	task9 = createTask(1, "Do other homework");
-	task10 = createTask(0, "Finish heap homework");
+    /* create tasks */
+    task1 = createTask(10, "Clean house");
+    task2 = createTask(6, "Eat dinner");
+    task3 = createTask(2, "Do laundry");
+    task4 = createTask(4, "Do dishes");
+    task5 = createTask(5, "Grocery shopping");
+    task6 = createTask(7, "Change oil");
+    task7 = createTask(13, "Do taxes");
+    task8 = createTask(8, "Sleep");
+    task9 = createTask(1, "Do other homework");
+    task10 = createTask(0, "Finish heap homework");
 
     printf("Beginning testing... \n");
 
     printf("------------------Testing addHeap------------------ \n");
-	/* add tasks to the dynamic array - first three inserts are tested*/
-	printf("Add 10 to heap\n");
-	addHeap(taskList, task1);
-	printTestResult(getDynArr(taskList, 0).priority == 10, "getDynArr", "Priority of index(0) == 10");
-	printf("Add 6 to heap\n");
-	addHeap(taskList, task2);
-	printTestResult(getDynArr(taskList, 0).priority == 6, "getDynArr", "Priority of index(0) == 6");
-	printTestResult(getDynArr(taskList, 1).priority == 10, "getDynArr", "Priority of index(1) == 10");
-	printf("Add 2 to heap\n");
-	addHeap(taskList, task3);
-	printTestResult(getDynArr(taskList, 0).priority == 2, "getDynArr", "Priority of index(0) == 2");
-	printTestResult(getDynArr(taskList, 1).priority == 10, "getDynArr", "Priority of index(1) == 10");
-	printTestResult(getDynArr(taskList, 2).priority == 6, "getDynArr", "Priority of index(2) == 6");
-	/* It is your job to test past this point*/
-	addHeap(taskList, task4);
-	addHeap(taskList, task5);
-	addHeap(taskList, task6);
-	addHeap(taskList, task7);
-	addHeap(taskList, task8);
-	addHeap(taskList, task9);
-	addHeap(taskList, task10);
+    /* add tasks to the dynamic array - first three inserts are tested*/
+    printf("Add 10 to heap\n");
+    addHeap(taskList, task1);
+    printTestResult(getDynArr(taskList, 0).priority == 10, "getDynArr", "Priority of index(0) == 10");
+    printf("Add 6 to heap\n");
+    addHeap(taskList, task2);
+    printTestResult(getDynArr(taskList, 0).priority == 6, "getDynArr", "Priority of index(0) == 6");
+    printTestResult(getDynArr(taskList, 1).priority == 10, "getDynArr", "Priority of index(1) == 10");
+    printf("Add 2 to heap\n");
+    addHeap(taskList, task3);
+    printTestResult(getDynArr(taskList, 0).priority == 2, "getDynArr", "Priority of index(0) == 2");
+    printTestResult(getDynArr(taskList, 1).priority == 10, "getDynArr", "Priority of index(1) == 10");
+    printTestResult(getDynArr(taskList, 2).priority == 6, "getDynArr", "Priority of index(2) == 6");
+    /* It is your job to test past this point*/
+    addHeap(taskList, task4);
+    addHeap(taskList, task5);
+    addHeap(taskList, task6);
+    addHeap(taskList, task7);
+    addHeap(taskList, task8);
+    addHeap(taskList, task9);
+    addHeap(taskList, task10);
 
-	printf("------------------Testing size------------------ \n");
-	/* Should print: heap size: 10 */
-	printTestResult(sizeDynArr(taskList) == 10, "sizeDynArr", "Size == 10");
+    printf("------------------Testing size------------------ \n");
+    /* Should print: heap size: 10 */
+    printTestResult(sizeDynArr(taskList) == 10, "sizeDynArr", "Size == 10");
 
-	printf("--------------printing heap-------------- \n");
-	/* Should print - 0 1 6 4 2 7 13 10 8 5 (with descriptions) */
-	printList(taskList);
+    printf("--------------printing heap-------------- \n");
+    /* Should print - 0 1 6 4 2 7 13 10 8 5 (with descriptions) */
+    printList(taskList);
 
-	printf("------------------Testing getMinHeap------------------ \n");
-	printTestResult(getMinHeap(taskList).priority == 0, "getMinHeap", "Priority == 0");
+    printf("------------------Testing getMinHeap------------------ \n");
+    printTestResult(getMinHeap(taskList).priority == 0, "getMinHeap", "Priority == 0");
 
-	printf("------------------Testing removeMinHeap------------------ \n");
-	removeMinHeap(taskList);
-	printTestResult(getMinHeap(taskList).priority == 1, "getMinHeap", "Priority == 1");
-	printTestResult(getDynArr(taskList, 1).priority == 2, "getDynArr", "Priority == 2");
-	printTestResult(getDynArr(taskList, 2).priority == 6, "getDynArr", "Priority == 6");
-	printTestResult(getDynArr(taskList, 3).priority == 4, "getDynArr", "Priority == 4");
-	printTestResult(getDynArr(taskList, 4).priority == 5, "getDynArr", "Priority == 5");
-	printTestResult(getDynArr(taskList, 5).priority == 7, "getDynArr", "Priority == 7");
-	printTestResult(getDynArr(taskList, 6).priority == 13, "getDynArr", "Priority == 13");
-	printTestResult(getDynArr(taskList, 7).priority == 10, "getDynArr", "Priority == 10");
-	printTestResult(getDynArr(taskList, 8).priority == 8, "getDynArr", "Priority == 8");
-	
-	printf("------------------Initial testing complete------------------ \n");
-	
-	return 0;
+    printf("------------------Testing removeMinHeap------------------ \n");
+    removeMinHeap(taskList);
+    printTestResult(getMinHeap(taskList).priority == 1, "getMinHeap", "Priority == 1");
+    printTestResult(getDynArr(taskList, 1).priority == 2, "getDynArr", "Priority == 2");
+    printTestResult(getDynArr(taskList, 2).priority == 6, "getDynArr", "Priority == 6");
+    printTestResult(getDynArr(taskList, 3).priority == 4, "getDynArr", "Priority == 4");
+    printTestResult(getDynArr(taskList, 4).priority == 5, "getDynArr", "Priority == 5");
+    printTestResult(getDynArr(taskList, 5).priority == 7, "getDynArr", "Priority == 7");
+    printTestResult(getDynArr(taskList, 6).priority == 13, "getDynArr", "Priority == 13");
+    printTestResult(getDynArr(taskList, 7).priority == 10, "getDynArr", "Priority == 10");
+    printTestResult(getDynArr(taskList, 8).priority == 8, "getDynArr", "Priority == 8");
+    
+    printf("------------------Initial testing complete------------------ \n");
+    
+    return 0;
 }
